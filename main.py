55ffba91339a5bd6f065170c3e0ef2c178d91cdf
kishoreaadada <kishorekotesh678@gmail.com>
2022-07-15 18:25:30 +0530

    moved driver function code to main function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from practiceScenarios import practiceFunctions
 
 
 def print_hi(name):
@@ -11,6 +12,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(practiceFunctions.calculator(12, 13))
+    print(practiceFunctions.calculator(24, 13, 'add'))
+    print(practiceFunctions.calculator('12', 45, 'subtract'))
+    print(practiceFunctions.calculator(34, 56, 'multiply'))
+    print(practiceFunctions.calculator(39, '13', 'divide'))
+    print(practiceFunctions.calculator(12, 13, 'all'))
+    print(practiceFunctions.calculator('12', '13', 'percent'))
+    print(practiceFunctions.greatestnumber(23, 45, 89, 93, 20, 10, 109, 0, 89))
+    print(practiceFunctions.greatestnumber(*range(100, 1000)))
+    practiceFunctions.stringactions("inceptez technologies")
